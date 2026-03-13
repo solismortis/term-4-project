91e69a4f11848a29d0b8b3bf24eb3b9417c3b47b
SolisMortis <callmesolis@gmail.com>
2026-03-13 22:35:44 +0500

    Revert "Revert this"
    This reverts commit 4d38d26d1fa9817a3a754fca12cfa8f98e724f9e.

--- term 4 project.cpp
+++ term 4 project.cpp
@@ -31,12 +31,14 @@ float random_float(float min, float max) {
     return min + static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / (max - min)));
 }
 
-struct Bacterium {
+class Bacterium {
     float m_x;
     float m_y;
     float m_speed;
     float m_max_energy;
     float m_energy; // Current energy
+public:
+    inline static std::vector<Bacterium*> bacteria_container;
 
     Bacterium() {
         /* Adams constructor */
@@ -45,6 +47,7 @@ struct Bacterium {
         m_speed = random_float(0.0f, ADAM_MAX_SPEED);
         m_max_energy = random_float(0.0f, ADAM_MAX_ENERGY);
         m_energy = m_max_energy;
+        bacteria_container.push_back(this);
     }
 
     Bacterium(Bacterium* parent) {
@@ -75,6 +78,9 @@ struct Bacterium {
 
         // Split parent energy
         m_energy = parent->m_energy / 2.0f;
+
+        // Add to static container
+        bacteria_container.push_back(this);
     }
 
     void move() {
@@ -83,13 +89,9 @@ struct Bacterium {
         // Move to it
     }
 
-    bool can_eat(Food* f) {
-        /* Takes food. Returns true if a bacterium is close enough to eat this food */
-        if (std::sqrt(std::pow(m_x - f->m_x, 2)
-                    + std::pow(m_y - f->m_y, 2)) <= f->m_radius) {
-            return true;
-        }
-        return false;
+    bool eating() {
+        /* Returns true if a bacterium is close enough to eat */
+
     }
 
     void eat() {
@@ -113,17 +115,20 @@ struct Bacterium {
     }
 };
 
-struct Food {
+class Food {
     float m_x;
     float m_y;
     float m_energy;
     float m_radius;
+public:
+    inline static std::vector<Food*> food_container;
 
     Food() {
         m_x = random_float(0.0f, static_cast<float>(LEVEL_SIZE));
         m_y = random_float(0.0f, static_cast<float>(LEVEL_SIZE));
         m_energy = random_float(FOOD_MIN, FOOD_MAX);
         m_radius = m_energy / 20.0f;
+        food_container.push_back(this);
     }
 
     ~Food() {
@@ -138,45 +143,23 @@ struct Food {
 int main()
 {
     srand(time(NULL)); // Random seed
-    std::vector<Bacterium> bacteria_container;
-    std::vector<Food> food_container;
-
-    // Creat adams
-    for (int i{}; i < ADAMS_N; ++i) {
-
-    }
 
     int step{};
-    float f_new_energy, f_prev_energy;
     while (step < STEPS) {
         std::cout << step << "\n";
         // Movement
-        for (Bacterium b : bacteria_container) {
-            b.move();
+        for (Bacterium* b : Bacterium::bacteria_container) {
+            b->move();
         }
 
-        // Update food and bacteria energy
-        for (Food f : food_container) {
-            std::vector<Bacterium*> consumers;
-            for (Bacterium b : bacteria_container) {
-                if (b.can_eat(f)) {
-                    consumers.push_back(b);
-                }
-            }
-            // Each bacteria eats max 1 energy
-            f_new_energy = std::max(0.0f, f->m_energy - consumers.size());
-            f_prev_energy = f->m_energy;
-            f->m_energy = f_new_energy;
-            // Delete empty food
-            if (f->m_energy <= 0) {
-                delete f;
-            }
-            // Update bacteria energy (food component)
-            for (Bacterium* b : consumers) {
-                b->m_energy += (f_prev_energy - f_new_energy) / consumers.size();
-            }
-        }
+        // Update food energy
+        std::vector<Bacterium*> eating_bacteria;
+        for (Bacterium* b : Bacterium::bacteria_container) {
 
+        }
+        
+        // Bacteria energy
+        
         // Death
         
         // Reproduction
